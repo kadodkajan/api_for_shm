dfdd07577cf88c6d8a619de4f2ecf267e8f73bfc
nava <sarvanava@gmail.com>
2024-01-10 11:23:43 -0500

    guide api finished

--- index.js
+++ index.js
@@ -555,7 +555,62 @@ app.post("/addguide", async (req, res) => {
     res.status(500).json({ status: "error", message: "Internal Server Error" });
   }
 });
+app.get("/getAllGuide", async (req, res) => {
+  try {
+    // Use Mongoose to find all stores
+    const allGuide = await Guide.find();
+    const transformedGuides = allGuide.map(guide => ({
+      _id:guide._id,
+      guideName: guide.guideName,
+     
+    }));
+    // Send the list of stores as a JSON response
+    res.json({ status: "success", guide: transformedGuides });
+  } catch (error) {
+    // Send an error response if an exception occurs
+    console.error("Error getting all guide:", error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+});
+// Get guide by ID
+app.get("/getGuideById/:id", async (req, res) => {
+  try {
+    // Use Mongoose to find the guide by ID
+    const guide = await Guide.findById(req.params.id);
 
+    // Check if the guide with the provided ID exists
+    if (!guide) {
+      return res.status(404).json({ status: "error", message: "Guide not found" });
+    }
+
+    // Send the guide as a JSON response
+    res.json({ status: "success", guide });
+  } catch (error) {
+    // Send an error response if an exception occurs
+    console.error("Error getting guide by ID:", error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+});
+
+// Delete guide by ID
+app.delete("/deleteGuideById/:id", async (req, res) => {
+  try {
+    // Use Mongoose to find and remove the guide by ID
+    const deletedGuide = await Guide.findByIdAndDelete(req.params.id);
+
+    // Check if the guide with the provided ID exists
+    if (!deletedGuide) {
+      return res.status(404).json({ status: "error", message: "Guide not found" });
+    }
+
+    // Send a success response with the deleted guide
+    res.json({ status: "success", message: "Guide  deleted successfully" });
+  } catch (error) {
+    // Send an error response if an exception occurs
+    console.error("Error deleting guide by ID:", error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+});
 
 mongoose
   .connect(DB)
