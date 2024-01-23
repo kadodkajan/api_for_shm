ac4ff4d39222917976595732a641834b33eb2633
Nava <sarvanava@gmail.com>
2024-01-23 10:05:41 -0500

    new features

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.use("/", teamRoutes);
 const procatRoutes = require("./src/routes/procatRoutes");
 app.use("/", procatRoutes);
 
+const productRoutes = require("./src/routes/productRoutes");
+app.use("/", productRoutes);
 
 
 app.post("/login", async (req, res) => {
@@ -75,112 +77,7 @@ app.post("/login", async (req, res) => {
 
 
 
-//Product
-const productSchema = new Schema({
-  ProductId: String,
-  productName: String,
-  procategory: procategorySchema, // Embedding procategorySchema as a subdocument
-});
 
-const Product = mongoose.model("Product", productSchema);
-
-app.post("/addproduct", async (req, res) => {
-  try {
-    const { ProductId, productName, procategory } = req.body;
-
-    // Check if the product category already exists
-    const existingCategory = await ProCat.findById(procategory);
-
-    if (!existingCategory) {
-      // If the category does not exist, create a new one
-      const newProCat = new ProCat(procategory);
-      await newProCat.save();
-    }
-    const existingProduct = await Product.findOne({ ProductId });
-
-    if (existingProduct) {
-      return res.status(400).json({
-        status: "error",
-        message: "Product with the given productId already exists",
-      });
-    }
-
-    // Create a new product using the provided information
-    const newProduct = new Product({
-      ProductId,
-      productName,
-      procategory: existingCategory, // Assign the procategory information to the product
-    });
-
-    await newProduct.save();
-
-    // Send a success response
-    res.json({ status: "success", message: "Product added successfully" });
-  } catch (error) {
-    // Send an error response if an exception occurs
-    console.error(error);
-    res.status(500).json({ status: "error", message: "Internal Server Error" });
-  }
-});
-
-app.get("/getAllProduct", async (req, res) => {
-  try {
-    // Use Mongoose to find all stores
-    const allProduct = await Product.find();
-
-    // Send the list of stores as a JSON response
-    res.json({ status: "success", product: allProduct });
-  } catch (error) {
-    // Send an error response if an exception occurs
-    console.error("Error getting all catergory:", error);
-    res.status(500).json({ status: "error", message: "Internal Server Error" });
-  }
-});
-app.get("/getProductbycate/:catId", async (req, res) => {
-  try {
-    const catId = req.params.catId;
-    const products = await Product.find({ "procategory._id": catId });
-
-    // Transform each product object to the desired format
-    const transformedProducts = products.map((product) => ({
-      _id: product._id,
-      ProductId: product.ProductId,
-      productName: product.productName,
-      procategory: product.procategory,
-    }));
-
-    // Send the transformed list of products as a JSON response
-    res.json({ status: "success", products: transformedProducts });
-  } catch (error) {
-    // Send an error response if an exception occurs
-    console.error("Error getting products by category:", error);
-    res.status(500).json({ status: "error", message: "Internal Server Error" });
-  }
-});
-
-// DELETE route to remove
-app.delete("/deleteproduct/:productId", (req, res) => {
-  const productId = req.params.productId;
-
-  Product.findOneAndDelete({ _id: productId })
-    .then((deletedPro) => {
-      if (deletedPro) {
-        res.json({
-          status: "success",
-          message: "Product  deleted successfully",
-        });
-      } else {
-        res
-          .status(404)
-          .json({ status: "error", message: "Product  not found" });
-      }
-    })
-    .catch((error) => {
-      res
-        .status(500)
-        .json({ status: "error", message: "Internal Server Error" });
-    });
-});
 
 const guideSchema = new Schema({
   guideName: String,
